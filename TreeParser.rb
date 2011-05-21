bf8831cbb9f1e03b1590a629a11298f536b89639
Chris Hunt <huntca@gmail.com>
2011-05-21 14:34:47 -0700

    Implemented ParseTabTree to parse tab trees

--- TreeParser.rb
+++ TreeParser.rb
@@ -1,8 +1,35 @@
-require 'ruby-debug'
+require 'tree'
 
 module TreeParser
   def self.ParseTabTree(tab_tree)
-    tab_tree
+    lines = []
+    # parse string into lines, saving tab indent
+    tab_tree.each_line do |line|
+      tabs = line.split("\t")
+      indent = tabs.size
+      name = tabs.last.strip
+      lines << {:indent => indent, :node => Tree::TreeNode.new(name)}
+    end
+    
+    # find the parent of each node
+    (0..lines.size-1).each do |i|
+      parent = i-1
+      while (lines[i][:indent] <= lines[parent][:indent]) && (parent > 0)
+        parent = parent - 1
+      end
+      lines[i][:parent] = parent
+    end
+
+    # create tree
+    tree = Tree::TreeNode.new("ROOT")
+    (0..lines.size-1).each do |i|
+      tree << lines[i][:node] if lines[i][:parent] == -1
+      lines.each do |line|
+        lines[i][:node] << line[:node] if line[:parent] == i
+      end
+    end
+
+    tree
   end
 end
 
@@ -16,4 +43,5 @@ tab_tree << "\n\t\tChild3"
 tab_tree << "\n\tParent2"
 tab_tree << "\n\t\tChild4"
 
-puts TreeParser::ParseTabTree tab_tree
+tree = TreeParser::ParseTabTree tab_tree
+tree.print_tree
